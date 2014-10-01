0ba6f08773068db1699498cd7af324d8cd6a53e6
John O'Keefe <jokeefe94@gmail.com>
2014-10-01 12:25:15 +0100

    Added registeration form, added jquery NextCapital client

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " jokeefe94. All rights reserved.</p>";
 
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='customers.html'>Data</a></li>" +
-"<li><a href='about.html'>About</a></li>" +
-"</ul>";
+
 
