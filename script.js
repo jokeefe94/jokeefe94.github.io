d7f6cb2ec58861e7adc97264a0fbd2a96690dfc3
John O'Keefe <jokeefe94@gmail.com>
2014-10-01 09:30:09 +0100

    Added validation of required input

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.getElementById("nav01").innerHTML =
 "<li><a href='customers.html'>Data</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
 "</ul>";
+
+function validateForm() {
+    var x = document.forms["logInForm"]["email"].value;
+    var y = document.forms["logInForm"]["password"].value;
+    if (x == null || x == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    else if (y == null || y == "") {
+        alert("Password must be filled out");
+        return false;
+    }
+
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+}
