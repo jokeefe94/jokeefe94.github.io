0b7eae1d7c6c3cb98a27668be2db2229f5a23667
Jack Miller <jackmiller403@gmail.com>
2014-11-23 14:59:49 +0100

    Working on adding directions

--- script.js
+++ script.js
@@ -108,7 +108,6 @@ function LondonBikes() {
 	function showAllStations() {
 		for (var i = 0; i < self.stations.length; i++) {
 			marker = self.stations[i].marker(google, self.map);
-			console.log("%O", marker);
 			self.markers.push(marker);
 		}
 	}
@@ -271,10 +270,6 @@ function LondonBikes() {
 			}
 
 		}
-	}
-
-	function showAllStations() {
-
 	}
 
 	function addTestButton() {
